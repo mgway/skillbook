8b6e4e1f326cfd3d441e042ac4e7426bb8bccc17
Matthew Galloway <mgalloway@silverbottle.net>
2014-08-07 02:56:37 +0000

    HTML email template

--- mail.py
+++ mail.py
@@ -5,20 +5,24 @@ import db
 import config
 
 
-def send(to, subject, body):
+def send(to, subject, text=None, html=None):
     url = 'https://api.mailgun.net/v2/%s/messages' % config.mail.url
-    return requests.post(
-        url,
-        auth=('api', config.mail.key),
-        data={'from': config.mail.from_address,
-              'to': to,
-              'subject': subject,
-              'text': body})
+    data = {'from': config.mail.from_address,
+            'to': to,
+            'subject': subject}
+    if text:
+        data['text'] = text
+        
+    if html:
+        data['html'] = html    
+
+    return requests.post(url, auth=('api', config.mail.key), data=data)
 
 
 def send_confirmation(user):
     loader = Loader('templates/mail')
-    body = loader.load('confirm.txt').generate(user=user, config=config.web)
+    text = loader.load('confirm.txt').generate(user=user, config=config.web)
+    html = loader.load('confirm.html').generate(user=user, config=config.web)
     
     if not user.unsubscribed:
-        send(user.email, 'Confirmation instructions', body)
+        send(user.email, 'Confirmation instructions', text=text, html=html)
